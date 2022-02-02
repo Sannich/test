3ea76eb0439c255955cc4a3f70175c6b26182d8c
Sannich <sasha.kozak.sasha@gmail.com>
2022-02-02 16:52:50 +0200

    sixth

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int b;
-        int c;
-        int ua;
+        double d = 0.3;
+        //создаем переменную і; цикл выполняеться пока І не будет равно = 20;в конце цикла і; збільшується на один
+        for (int i = 1; i <= 20; i++) {
+            System.out.println("Hello World" + i);
+            System.out.println(++d);
+
+        }
     }
 }
